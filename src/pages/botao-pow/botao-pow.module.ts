f07b2c9daf6561ec0665b367f722b0b8b8ef7e2c
adhamlucas <adhamlucas20@gmail.com>
2018-12-10 14:09:30 -0400

    nova page

---
+++ src/pages/botao-pow/botao-pow.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { BotaoPowPage } from './botao-pow';
+
+@NgModule({
+  declarations: [
+    BotaoPowPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(BotaoPowPage),
+  ],
+})
+export class BotaoPowPageModule {}
